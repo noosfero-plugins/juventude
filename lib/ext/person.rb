82f8f051425a046bb144f831dcb1a947334d972d
Caio Almeida <caiosba@gmail.com>
2015-09-08 17:37:59 -0300

    refactoring load state and city

--- lib/ext/person.rb
+++ lib/ext/person.rb
@@ -6,17 +6,4 @@ class Person
 
   attr_accessible :orientacao_sexual, :identidade_genero, :transgenero, :tipo, :etnia
 
-  def city= city
-    city = City.find(city) unless city.kind_of?(City)
-    self.region = city
-  end
-
-  def city
-    self.region
-  end
-
-  def state
-    self.region.parent if self.region
-  end
-
 end
--- lib/juventude_plugin/api.rb
+++ lib/juventude_plugin/api.rb
@@ -7,10 +7,20 @@ class JuventudePlugin::API < Grape::API
       present states
     end
 
+    get ':id' do
+      state = State.select([:id, :name]).find(params[:id])
+      present state
+    end
+
     get ':id/cities' do
       state = State.find(params[:id])
       cities = City.where(:parent_id => state.id).select([:id, :name]).order(:name)
       present cities
+    end
+
+    get ':id/cities/:city_id' do
+      city = City.select([:id, :name]).find(params[:city_id])
+      present city
     end
 
   end
