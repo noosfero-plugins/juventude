1fe29568d853971cd91fc65025d618cb6b43b05b
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-10-14 14:33:00 -0300

    avoid juventud specific definition impact units tests

--- lib/ext/entities.rb
+++ lib/ext/entities.rb
@@ -5,7 +5,20 @@ module Noosfero
     module Entities
 
       class Person < Profile
-        expose :orientacao_sexual, :identidade_genero, :transgenero, :etnia, :points, :level, :gamification_plugin_level_percent
+        expose :orientacao_sexual, :identidade_genero, :transgenero, :etnia
+
+        expose :points do |person, options|
+          person.points if person.respond_to?(:points) 
+        end
+
+        expose :level do |person, options|
+          person.level if person.respond_to?(:level) 
+        end
+
+        expose :gamification_plugin_level_percent do |person, options|
+          person.gamification_plugin_level_percent if person.respond_to?(:gamification_plugin_level_percent) 
+        end
+
       end
 
     end
--- lib/juventude_plugin.rb
+++ lib/juventude_plugin.rb
@@ -1,20 +1,20 @@
 class JuventudePlugin < Noosfero::Plugin
 
-  def self.plugin_name
-    _('Juventude custom plugin')
-  end
-
-  def self.plugin_description
-    _("Provide a plugin to juventude environment.")
-  end
-
-  def self.api_mount_points
-    [JuventudePlugin::API]
-  end
-
-  def control_panel_buttons
-    {:title => _('Generate Report'), :icon => 'report', :url => {:controller => 'juventude_plugin_myprofile', :action => :send_report}, :html_options => {:class => 'admin-report'}} if context.send(:current_person).is_admin?
-
-  end
+#  def self.plugin_name
+#    _('Juventude custom plugin')
+#  end
+#
+#  def self.plugin_description
+#    _("Provide a plugin to juventude environment.")
+#  end
+#
+#  def self.api_mount_points
+#    [JuventudePlugin::API]
+#  end
+#
+#  def control_panel_buttons
+#    {:title => _('Generate Report'), :icon => 'report', :url => {:controller => 'juventude_plugin_myprofile', :action => :send_report}, :html_options => {:class => 'admin-report'}} if context.send(:current_person).is_admin?
+#
+#  end
 
 end
