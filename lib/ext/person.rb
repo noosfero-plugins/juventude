3d9b5b5701497a89638c035ce987a02eba0309d3
Hugo Melo <hugo@riseup.net>
2015-10-14 17:12:25 -0300

    Add check for profile completion. hardcoded for juv

--- lib/ext/person.rb
+++ lib/ext/person.rb
@@ -6,6 +6,17 @@ class Person
 
   attr_accessible :orientacao_sexual, :identidade_genero, :transgenero, :tipo, :etnia
 
+  def is_profile_complete?
+    #FIXME: this check is hardcoded to satisfy a temporary issue.
+    #a better fix is being implemented already
+    !(self.name.blank? or
+      (self.data[:identidade_genero].blank? and self.data[:transgenero].blank?) or
+      self.data[:etnia].blank? or
+      self.data[:orientacao_sexual].blank? or
+      self.data[:state].blank? or
+      self.data[:city].blank?)
+  end
+
   def conference_comments
     Profile['conferencia'].comments_received.where(author_id: id)
   end
